796e486c61b937e91e8ddd7bf99f94a11ed70e4c
sanjiv kumar ojha <sanjiv.bapi@gmail.com>
2017-09-05 16:12:14 +0530

    Delete device.py

--- app/model/device.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column
-from sqlalchemy import String, Integer, LargeBinary
-from sqlalchemy.dialects.postgresql import JSONB
-
-from app.model import Base
-from app.config import UUID_LEN
-from app.utils import alchemy
